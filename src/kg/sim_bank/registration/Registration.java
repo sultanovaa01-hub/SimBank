a2425ad9509e9d6db242f933c5ae3f2e3bcb0283
Aliya Sultanova <aliyasultanova@Aliyas-MacBook-Air.local>
2026-03-24 03:53:49 +0600

    Registration v2

--- src/kg/sim_bank/registration/Registration.java
+++ src/kg/sim_bank/registration/Registration.java
@@ -3,5 +3,6 @@ package kg.sim_bank.registration;
 public class Registration {
     static void main() {
         System.out.println("Registration");
+        System.out.println("Registration v2");
     }
 }
