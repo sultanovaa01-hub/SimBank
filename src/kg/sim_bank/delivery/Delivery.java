33c923c270285bc218daba6fcb04177e77ce0a40
Aliya Sultanova <sultanovaa01@gmail.com>
2026-03-24 17:03:14 +0600

    Resolved Conflict

---
+++ src/kg/sim_bank/delivery/Delivery.java
@@ -0,0 +1,8 @@
+package kg.sim_bank.delivery;
+
+public class Delivery {
+    static void main() {
+        System.out.println("delivery Yandex");
+        System.out.println("delivery Glovo");
+    }
+}
