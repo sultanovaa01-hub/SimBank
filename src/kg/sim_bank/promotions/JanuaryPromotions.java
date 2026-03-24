df6f484ba956bf92e4e6e7192e874105fdaaae4a
sultanovaa01-hub <sultanovaa01@gmail.com>
2026-03-24 17:25:17 +0600

    January promo123
    JAnuary promotion1
    
    JAnuary promotion2
    
    JAnuary promotion3
    
    JAnuary promotion4
    
    JAnuary promotion5
    
    JAnuary promotion6

---
+++ src/kg/sim_bank/promotions/JanuaryPromotions.java
@@ -0,0 +1,14 @@
+package kg.sim_bank.promotions;
+
+public class JanuaryPromotions {
+    static void main() {
+        System.out.println("JAnuary Promotion");
+        System.out.println("JAnuary Promotion123");
+        System.out.println("JAnuary Promotion1234");
+        System.out.println("JAnuary Promotion12345");
+        System.out.println("JAnuary Promotion123456");
+        System.out.println("JAnuary Promotion1234567");
+        System.out.println("JAnuary Promotion12345678");
+        System.out.println("JAnuary Promotion123456789");
+    }
+}
