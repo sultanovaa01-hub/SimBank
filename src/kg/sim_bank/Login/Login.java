4f8f0682a2e4de3405ac76a86aa3c16aa08dcc08
Alexey Kozlov <aliya-honey@inbox.ru>
2026-03-26 17:00:31 +0600

    fixed pr comments

--- src/kg/sim_bank/Login/Login.java
+++ src/kg/sim_bank/Login/Login.java
@@ -1,8 +1,7 @@
 package kg.sim_bank.Login;
-
 public class Login {
     static void main() {
-        System.out.println("login");
+        System.out.println("Login");
         System.out.println("login2");
     }
 }
